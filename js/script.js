b0fb770c62a32a71c4e824a73e9567791ca52a3d
Tareq <razzbabut@gmail.com>
2024-02-15 02:46:40 +0600

    updated score and play again with fresh game page

--- js/script.js
+++ js/script.js
@@ -6,20 +6,55 @@
 //     const secondPage = document.getElementById('second-page');
 //     secondPage.classList.remove('hidden')
 // }
+
 function addKeyboardPress(event) {
     const keyElementPressed = event.key;
+
+    // exit game with 'Esc' button
+    if(keyElementPressed === 'Escape'){
+        gameOver();
+    }
 
     const currentAlphabet = document.getElementById("text-display");
     const alphabets = currentAlphabet.innerText;
 
 
     if (keyElementPressed === alphabets.toLocaleLowerCase()) {
-        console.log("matched")
+        // console.log("matched")
         playGame();
         removeBackgroundColor(keyElementPressed);
+
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // const updateScore = currentScore + 1;
+
+        // currentScoreElement.innerText = updateScore;
+
+        const currentScore = getTextValueById('current-score');
+        const updateScore = currentScore + 1;
+
+        setTextValueById('current-score', updateScore)
+
     }
     else {
-        console.log('not matched')
+        // console.log('not matched')
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+        // const updateLife = currentLife - 1;
+
+        // currentLifeElement.innerText = updateLife;
+
+        const currentLife = getTextValueById('current-life');
+        const updateLife = currentLife - 1;
+
+        setTextValueById('current-life', updateLife)
+
+        if (updateLife === 0) {
+            gameOver();
+        }
+
     }
 
 }
@@ -34,13 +69,33 @@ function playGame() {
     currentAlphabet.innerText = alphabet;
 
     setBackgroundColor(alphabet);
-
 }
 
 // Enter the game
 function startGame() {
+    // hide pages and show page
     hidePagesById('first-page');
+    hidePagesById('third-page');
     showPagesById('second-page');
 
+    // update life and score
+    setTextValueById('current-life', 5);
+    setTextValueById('current-score', 0);
+
+    setTextValueById('last-score', 0)
+
     playGame();
+}
+
+// game over
+function gameOver() {
+    hidePagesById('second-page');
+    showPagesById('third-page');
+
+    const lastScore = getTextValueById('current-score');
+    setTextValueById('last-score', lastScore);
+
+    // remove last element background
+    const lastElement = getElementTextById('text-display');
+    removeBackgroundColor(lastElement);
 }
--- js/utility.js
+++ js/utility.js
@@ -18,9 +18,31 @@ function getAlphabet() {
 
 function setBackgroundColor(elementId) {
     const element = document.getElementById(elementId);
-    element.classList.add('bg-orange-400');
+    element.classList.add('bg-orange-500');
 }
 function removeBackgroundColor(elementId) {
     const element = document.getElementById(elementId);
-    element.classList.remove('bg-orange-400');
+    element.classList.remove('bg-orange-500');
 }
+
+function getTextValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const updateElement = element.innerText;
+    const getValue = parseInt(updateElement);
+    return getValue;
+}
+
+function setTextValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+
+}
+
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const lastElement = element.innerText;
+    return lastElement;
+}
+
+
